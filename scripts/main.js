2dace4da82b025dccf0d154844fa85b261d7a46a
MonteDeveloper <lorenzomontello83@gmail.com>
2023-04-05 16:39:31 +0200

    fix: some mobile issues

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-// let thisYear = 2023;
-// let userName = prompt("Inserisci il tuo NOME").toLowerCase();
-// let userSurname = capitalizeWord(prompt("Inserisci il tuo COGNOME"));
-// let userfavColor = capitalizeWord(prompt("Inserisci il tuo COLORE PREFERITO"));
+let thisYear = 2023;
+let userName = prompt("Inserisci il tuo NOME").toLowerCase();
+let userSurname = capitalizeWord(prompt("Inserisci il tuo COGNOME"));
+let userfavColor = capitalizeWord(prompt("Inserisci il tuo COLORE PREFERITO"));
 
 alert(`Ciao ${userName}! Sto generando la tua password...`);
 
@@ -10,7 +10,7 @@ console.log(userName);
 if (userName == "" || userSurname == "" || userfavColor == "") {
     document.getElementById("passResult").innerHTML = 
     `
-    <span class="badge text-bg-danger fs-4">
+    <span class="badge text-bg-danger fs-4 text-wrap mw-100">
         ERRORE: ricarica la pagina e inserisci tutti i dati
     </span>
     `;
@@ -18,7 +18,7 @@ if (userName == "" || userSurname == "" || userfavColor == "") {
 }else{
     document.getElementById("passResult").innerHTML = 
     `
-    <span class="badge text-bg-success fs-4">
+    <span class="badge text-bg-success fs-4 text-wrap mw-100">
         ${userName.replace(' ', '')}${userSurname.replace(' ', '')}${userfavColor.replace(' ', '')}${thisYear}
     </span>
     `;
