eeca1d17a2bb19fda39b902d3fc7f9742eeb9958
MonteDeveloper <lorenzomontello83@gmail.com>
2023-04-05 15:16:44 +0200

    feat: bad password generator script

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+let thisYear = 2023;
+let userName = prompt("Inserisci il tuo NOME").toLowerCase();
+let userSurname = capitalizeWord(prompt("Inserisci il tuo COGNOME"));
+let userfavColor = capitalizeWord(prompt("Inserisci il tuo COLORE PREFERITO"));
+
+alert(`Ciao ${userName}! Sto generando la tua password...`);
+
+console.log(userName);
+
+if (userName == "" || userSurname == "" || userfavColor == "") {
+    document.getElementById("passResult").innerHTML = 
+    `
+    <span class="badge text-bg-danger fs-4">
+        ERRORE: ricarica la pagina e inserisci tutti i dati
+    </span>
+    `;
+}else{
+    document.getElementById("passResult").innerHTML = 
+    `
+    <span class="badge text-bg-success fs-4">
+        ${userName.replace(' ', '')}${userSurname.replace(' ', '')}${userfavColor.replace(' ', '')}${thisYear}
+    </span>
+    `;
+}
+
+function capitalizeWord(word){
+    word = word.trim();
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
